7f5a092e5ecccb73b2d2d6ffc94057f0b9138fbe
maks.iiim <dziadziukmaksim@gmail.com>
2022-09-06 20:57:35 +0300

    Initial commit

---
+++ app/src/main/java/DMV/fit/bstu/by/MainActivity.java
@@ -0,0 +1,14 @@
+package DMV.fit.bstu.by;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//TODO Add new method
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
